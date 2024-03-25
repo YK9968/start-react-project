e0db5ab585ea99d44892f169f025faf52b2c199c
yurii <robka4an@gmail.com>
2024-03-25 10:04:14 +0200

    props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,13 @@
-export const Product = () => {
-  const price = 999;
-  const imgUrl =
-    "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
-
-  return (
-    <>
-      <h2>Tacos</h2>
-      <img src={imgUrl} alt="Tacos With Lime" width="640" />
-      <p>Price: {price} credits</p>
-    </>
-  );
-};
+const Product = ({
+  name,
+  imgUrl = "https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder",
+  price,
+}) => (
+  <div>
+    <h2>{name}</h2>
+    <img src={imgUrl} alt={name} width="480" />
+    <p>Price: {price} credits</p>
+  </div>
+);
 
 export default Product;
