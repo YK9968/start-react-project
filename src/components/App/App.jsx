9c0516aae615bb235034e82476874713495ed0f8
yurii <robka4an@gmail.com>
2024-04-02 14:51:20 +0300

    finish last lesson module 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,28 @@
+import { useState, useEffect } from "react";
+
 export default function App() {
-  return <div></div>;
+  const [cliks, setcliks] = useState(() => {
+    const savedClicks = window.localStorage.getItem("saved-cliks");
+    if (savedClicks !== null) {
+      return savedClicks;
+    }
+    return 0;
+  });
+
+  const handleClickAdd = () => {
+    setcliks(cliks + 1);
+  };
+  const handleClickReset = () => {
+    setcliks(0);
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("saved-cliks", cliks);
+  }, [cliks]);
+  return (
+    <>
+      <button onClick={handleClickAdd}>Add: {cliks}</button>
+      <button onClick={handleClickReset}>Reset</button>
+    </>
+  );
 }
