f11817d10b620143b29c102e8fa25b9b57716aa4
yurii <robka4an@gmail.com>
2024-04-05 17:00:22 +0300

    form radio and checkBox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,78 @@
 // import LoginForm from "../LoginForm/LoginForm";
 // import MyComponets from "../MyComponents/MyComponents";
 import { useState } from "react";
-import LangSwitcher from "../LangSwitcher/LangSwitcher";
-import RenderFlag from "../RenderFlag/RenderFlag";
+// import LangSwitcher from "../LangSwitcher/LangSwitcher";
+// import RenderFlag from "../RenderFlag/RenderFlag";
 
 // import SearchBar from "../SearchBar/SearchBar";
 
 export default function App() {
-  const [lang, setLang] = useState("uk");
+  // const [coffeeSize, setCoffeeSize] = useState("sm");
+
+  // const handleChangeizeCoffee = (evt) => {
+  //   setCoffeeSize(evt.target.value);
+  //   console.log(evt.target.value);
+  // };
+  // const [lang, setLang] = useState("uk");
 
   // const handleLogin = (userData) => {
   //   console.log(userData);
   // };
 
+  const [hasAccepted, setHasAccepted] = useState(false);
+  const handleChange = (evt) => {
+    setHasAccepted(evt.target.checked);
+  };
+
   return (
     <div>
-      <p>Selected language: {lang}</p>
+      <label>
+        <input
+          type="checkbox"
+          name="terms"
+          checked={hasAccepted}
+          onChange={handleChange}
+        />
+        I accept terms and conditions
+      </label>
+      <button type="button" disabled={!hasAccepted}>
+        Proceed
+      </button>
+      {/* <h1>Select coffe size</h1>
+      <label>
+        <input
+          checked={coffeeSize === "sm"}
+          type="radio"
+          name="coffeeSize"
+          value="sm"
+          onChange={handleChangeizeCoffee}
+        />
+        Smal
+      </label>
+      <label>
+        <input
+          checked={coffeeSize === "md"}
+          type="radio"
+          name="coffeeSize"
+          value="md"
+          onChange={handleChangeizeCoffee}
+        />
+        Medium
+      </label>
+      <label>
+        <input
+          checked={coffeeSize === "lg"}
+          type="radio"
+          name="coffeeSize"
+          value="lg"
+          onChange={handleChangeizeCoffee}
+        />
+        Large
+      </label> */}
+      {/* <p>Selected language: {lang}</p>
 
       <LangSwitcher value={lang} onSelect={setLang} />
-      <RenderFlag langData={lang} />
+      <RenderFlag langData={lang} /> */}
       {/* <SearchBar /> */}
       {/* <h1>Please login to your account!</h1>
       <LoginForm onLogin={handleLogin} />
