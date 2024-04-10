c58dab575388db1f357e7e17dfe527f8457d1b4e
yurii <robka4an@gmail.com>
2024-04-10 19:02:23 +0300

    start 4 module

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,31 +1,62 @@
-import TaskList from "../TaskList/TaskList";
-import initialTasks from "../../todo.json";
-import Form from "../Form/Form";
-import Filter from "../Filter/Filter";
-import css from "./App.module.css";
-import { useState } from "react";
+// import TaskList from "../TaskList/TaskList";
+// import initialTasks from "../../todo.json";
+// import Form from "../Form/Form";
+// import Filter from "../Filter/Filter";
+// import css from "./App.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Articles from "../Articles/Articles";
 
 export default function App() {
-  const [tasks, setTasks] = useState(initialTasks);
-  const [filter, setFilter] = useState("");
-  const addTask = (newTask) => {
-    setTasks((prevTask) => {
-      return [...prevTask, newTask];
-    });
-  };
-  const deleteTask = (taskId) => {
-    setTasks((prevTask) => {
-      return prevTask.filter((task) => task.id !== taskId);
-    });
-  };
-  const visibleTasks = tasks.filter((task) =>
-    task.text.toLowerCase().includes(filter.toLowerCase())
-  );
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchArticles() {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          "https://hn.algolia.com/api/v1/search?query=react"
+        );
+        setArticles(response.data.hits);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchArticles();
+  }, []);
+  // const [tasks, setTasks] = useState(initialTasks);
+  // const [filter, setFilter] = useState("");
+  // const addTask = (newTask) => {
+  //   setTasks((prevTask) => {
+  //     return [...prevTask, newTask];
+  //   });
+  // };
+  // const deleteTask = (taskId) => {
+  //   setTasks((prevTask) => {
+  //     return prevTask.filter((task) => task.id !== taskId);
+  //   });
+  // };
+  // const visibleTasks = tasks.filter((task) =>
+  //   task.text.toLowerCase().includes(filter.toLowerCase())
+  // );
   return (
-    <div className={css.container}>
-      <Form onAdd={addTask} />
-      <Filter value={filter} onFilter={setFilter} />
-      <TaskList tasks={visibleTasks} onDelete={deleteTask} />
+    <div>
+      <h1>Latest articles</h1>
+      {loading && <p>Loading data, please wait...</p>}
+      {articles.length > 0 && <Articles items={articles} />}
+      {error && (
+        <p>Whoops, something went wrong! Please try reloading this page!</p>
+      )}
     </div>
   );
+  // <div className={css.container}>
+  //   <Form onAdd={addTask} />
+  //   <Filter value={filter} onFilter={setFilter} />
+  //   <TaskList tasks={visibleTasks} onDelete={deleteTask} />
+  // </div>
 }
