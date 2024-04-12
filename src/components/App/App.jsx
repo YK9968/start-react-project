15fc3662386704d5d83d03e332bcc85682595e27
yurii <robka4an@gmail.com>
2024-04-12 09:52:17 +0300

    finis module 4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,19 @@
 // import Filter from "../Filter/Filter";
 // import css from "./App.module.css";
 
-import { useState } from "react";
-import Error from "../Error/Error";
-import Loader from "../Loader/Loader";
-import SearchForm from "../SearchForm/SearchForm";
-import { fetchArticlesWithTopic } from "../../articles-api";
-import ArticleList from "../ArticleList/ArticleList";
+// import { useState } from "react";
+// import Error from "../Error/Error";
+// import Loader from "../Loader/Loader";
+// import SearchForm from "../SearchForm/SearchForm";
+// import { fetchArticlesWithTopic } from "../../articles-api";
+// import ArticleList from "../ArticleList/ArticleList";
+
+import Player from "../Player/Player";
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  // const [articles, setArticles] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
   // const [tasks, setTasks] = useState(initialTasks);
   // const [filter, setFilter] = useState("");
   // const addTask = (newTask) => {
@@ -30,28 +32,46 @@ export default function App() {
   // const visibleTasks = tasks.filter((task) =>
   //   task.text.toLowerCase().includes(filter.toLowerCase())
   // );
-  const handleSearch = async (topic) => {
-    try {
-      setArticles([]);
-      setLoading(true);
-      setError(false);
-      const data = await fetchArticlesWithTopic(topic);
-      setArticles(data);
-    } catch {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
+  // const handleSearch = async (topic) => {
+  //   try {
+  //     setArticles([]);
+  //     setLoading(true);
+  //     setError(false);
+  //     const data = await fetchArticlesWithTopic(topic);
+  //     setArticles(data);
+  //   } catch {
+  //     setError(true);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
+  // const [planets, setPlanets] = useState(["Earth", "Mars", "Jupiter", "Venus"]);
+  // const [query, setQuery] = useState("");
+  // const [clicks, setClicks] = useState(0);
+
+  // const filteredPlanets = planets.filter((planet) => planet.includes(query));
 
   return (
     <div>
-      <SearchForm onSubmit={handleSearch} />
-      {loading && <Loader />}
-      {error && <Error />}
-      {articles.length > 0 && <ArticleList items={articles} />}
+      <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
     </div>
   );
+
+  // <div>
+  //   <button onClick={() => setClicks(clicks + 1)}>
+  //     Number of clicks: {clicks}
+  //   </button>
+  //   <ul>
+  //     {filteredPlanets.map((planet) => (
+  //       <li key={planet}>{planet}</li>
+  //     ))}
+  //   </ul>
+  //   <SearchForm onSubmit={handleSearch} />
+  //   {loading && <Loader />}
+  //   {error && <Error />}
+  //   {articles.length > 0 && <ArticleList items={articles} />}
+  // </div>
   // <div className={css.container}>
   //   <Form onAdd={addTask} />
   //   <Filter value={filter} onFilter={setFilter} />
