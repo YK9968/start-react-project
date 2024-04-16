d8d2d13b4e31de1e725348075358f033104726f1
yurii <robka4an@gmail.com>
2024-04-16 13:59:39 +0300

    hard theme

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,12 +1,27 @@
 import { ProductList } from "../../components/ProductList/ProductList";
+import { SearchBox } from "../../components/SearchBox/SearchBox";
 import { getProducts } from "../../fakeApi";
+import { useSearchParams } from "react-router-dom";
 
 export default function Products() {
   const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const productName = searchParams.get("name") ?? "";
+
+  const visibleProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateQueryString = (value) => {
+    const nextParams = value !== "" ? { name: value } : {};
+    setSearchParams(nextParams);
+  };
 
   return (
     <main>
-      <ProductList products={products} />
+      <SearchBox value={productName} onChange={updateQueryString} />
+      <ProductList products={visibleProducts} />
     </main>
   );
 }
